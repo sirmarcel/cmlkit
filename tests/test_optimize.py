f80b0e5aa1083be9c426085b063f97031b0ec86c
Marcel Langer <me@sirmarcel.com>
2017-11-16 13:25:54 +0100

    Implement basic Bayesian optimiser

---
+++ tests/test_optimize.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+import numpy as np
+from optimize import *
+
+
+class TestFindsMinimum(TestCase):
+
+    def test_squared(self):
+        def myf(x, y):
+            return (x-1.0)**2.0 + (y-2.0)**2.0
+
+        params = {'y': (-3.0, 3.0), 'x': (-5.0, 5.0)}
+
+        result = optimize_bayes(myf, params)
+
+        optimized_params = result['params']
+        xy = np.array([optimized_params['x'], optimized_params['y']])
+
+        expected = np.array([1.0, 2.0])
+
+        np.testing.assert_allclose(xy, expected, atol=0.01)
+
+
+
+class TestWrapFunctionWithNamedArgs(TestCase):
+
+    def test_two_args(self):
+        param_names = ['a', 'b']
+
+        def myf(a, b):
+            return a-b
+
+        wrapped = wrap_function_with_named_args(myf, param_names)
+
+        self.assertEqual(wrapped(np.array([3, 2])), 1)
+
+
+    def test_three_args(self):
+        param_names = ['a', 'b', 'c']
+
+        def myf(a, b, c):
+            return (a-b)*c
+
+        wrapped = wrap_function_with_named_args(myf, param_names)
+
+        self.assertEqual(wrapped(np.array([3, 2, 5])), 5)
