462480b1a6bda1fe77c4ff875e6baecbdce265a5
Marcel Langer <me@sirmarcel.com>
2017-11-17 11:32:57 +0100

    Add basic io

---
+++ io.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def save(outfile, d):
+    """Save the dictionary d to oufile.
+
+    Note that .npy will automatically be appended to the filename.
+    """
+
+    np.save(outfile, d)
+
+
+def read(file):
+    """Load numpy data stored in file.
+
+    Note that .npy will automatically be appended to the filename.
+    """
+
+    d = np.load(file + '.npy').item()
+    return d
