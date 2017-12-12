514db0474ec20c1621c010d27f517c4ea6ee827a
Marcel Langer <me@sirmarcel.com>
2017-12-12 16:55:36 +0100

    add fourway split

--- indices.py
+++ indices.py
@@ -1,8 +1,49 @@
 import numpy as np
 
 
+def fourway_split(n, k_train, k_validate, k_test):
+    """Split the total n indices into four subsets.
+
+    Items are picked at random and not repeated.
+
+    Args:
+        n: Total number of indices
+        k_train: Number of items in the first set
+        k_validate: Number of items in the second set
+        k_test: Number of items in the third set
+
+    Returns:
+        train: First set
+        validate: Second set
+        test: Third set
+        rest: Fourth set with remaining items
+    """
+
+    full = np.arange(n)
+    rest, train = generate_distinct_sets(full, k_train)
+    rest, validate = generate_distinct_sets(rest, k_validate)
+    rest, test = generate_distinct_sets(rest, k_test)
+
+    return train, validate, test, rest
+
+
 def threeway_split(n, k_validate, k_test):
-    """Generate training, validation and training index sets for n items."""
+    """Generate training, validation and training index sets for n items.
+
+    Items are picked at random and not repeated.
+
+    In this split, the remaining items are returned first.
+
+    Args:
+        n: Total number of indices
+        k_validate: Number of items in the second set
+        k_test: Number of items in the third set
+
+    Returns:
+        train: First set (remaining items)
+        validate: Second set
+        test: Third set
+    """
 
     full = np.arange(n)
     model_building, test = generate_distinct_sets(full, k_test)
--- tests/test_indices.py
+++ tests/test_indices.py
@@ -2,7 +2,34 @@ from unittest import TestCase
 from indices import *
 
 
-class TestSplitIndices(TestCase):
+class TestFourwaySplit(TestCase):
+
+    def test_element_numbers_sum_up(self):
+        n = 40
+        k_test = 5
+        k_valid = 15
+        k_train = 7
+
+        a, b, c, d = fourway_split(n, k_train, k_test, k_valid)
+
+        self.assertEqual(len(a) + len(b) + len(c) + len(d), n)
+
+    def test_union_is_all(self):
+        n = 100
+        k_test = 25
+        k_valid = 15
+        k_train = 36
+
+        a, b, c, d = fourway_split(n, k_train, k_test, k_valid)
+
+        union = np.union1d(a, b)
+        union = np.union1d(union, c)
+        union = np.union1d(union, d)
+
+        self.assertEqual(union.all(), np.array(np.arange(n)).all())
+
+
+class TestThreewaySplit(TestCase):
 
     def test_element_numbers_sum_up(self):
         n = 30
