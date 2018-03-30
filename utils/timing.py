4eed0ecfb2e5ad9db9226a56a85c69b0f097de23
Marcel Langer <me@sirmarcel.com>
2018-03-30 16:50:38 +0200

    Add caching and timing utilities

---
+++ utils/timing.py
@@ -0,0 +1,20 @@
+import time
+
+
+def timerfunc(func):
+    """
+    A timer decorator
+    """
+    def function_timer(*args, **kwargs):
+        """
+        A nested function for timing other functions
+        """
+        start = time.time()
+        value = func(*args, **kwargs)
+        end = time.time()
+        runtime = end - start
+        msg = "The runtime for {func} took {time} seconds to complete"
+        print(msg.format(func=func.__name__,
+                         time=runtime))
+        return value, runtime
+    return function_timer
