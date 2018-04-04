f02a405f37fc201418e25a1fdca15db757e7d5a4
Marcel Langer <me@sirmarcel.com>
2018-04-04 15:15:05 +0200

    YOLO postprocessing

--- autotune/parallel.py
+++ autotune/parallel.py
@@ -35,7 +35,14 @@ def run_parallel(d, db):
                                      d['opt']['n_calls'],
                                      objective_with_loading, trials)
 
-    data = load(d['data']['id'])
+    to_save = {
+        'db': db,
+        'final_loss': trials.best_trial['result']['loss'],
+        'final_loss_variance': trials.best_trial['result']['loss_variance'],
+        'duration': duration
+    }
+
+    qmtio.save('out/' + d['name'] + '.run', to_save)
 
     top_3 = (np.array(trials.losses())).argsort()[:3]
 
@@ -44,15 +51,35 @@ def run_parallel(d, db):
         spec.name += '-' + 'best' + str(i)
         spec.save('out/')
 
-        best_mbtr = DiskAndMemCachedMBTR(data, spec)
-        best_mbtr.save('out/')
+    for i in range(3):
+        spec = ModelSpec.from_dict(trials.trials[top_3[i]]['result']['spec_dict'])
+        spec.name += '-' + 'best' + str(i)
 
-    to_save = {
-        'db': db,
-        'final_loss': trials.best_trial['result']['loss'],
-        'final_loss_variance': trials.best_trial['result']['loss_variance'],
-        'duration': duration
-    }
+        # The following is extremely ugly
+        if 'post' in d.keys():
+            for post_config in d['post']:
+
+                post_data_train = load(post_config['data_train']['id'])
+                post_data_test = load(post_config['data_test']['id'])
+
+                if post_data_train.parent_info['id'] == post_data_test.parent_info['id']:
+                    parent = load(post_data_train.parent_info['id'])
+                    idx_train = post_data_train.idx
+                    idx_test = post_data_test.idx
+
+                    rep = DiskAndMemCachedMBTR(parent, spec, name=spec.name)
+                    rep.save('out/')
+
+                    l = qmtr.loss(parent, spec, idx_train, idx_test, lossf=post_config['loss'], target_property=d['data']['property'])
+
+                    info = "Training %i model on %s and predicting %s gave loss %s" % (i, post_config['data_train']['id'], post_config['data_test']['id'], str(l))
+
+                    logging.info(info)
+
+                    key = 'model' + str(i) + '_' + post_config['data_train']['id'] + '->' + post_config['data_test']['id']
+
+                    to_save['post'][key] = l
+
 
     qmtio.save('out/' + d['name'] + '.run', to_save)
 
