e05156ec08a78c53a1d944ce843d1c51bd8dbbd9
Marcel Langer <me@sirmarcel.com>
2018-04-17 17:51:38 +0200

    Add additional regression functions with separate training and prediction sets/reps Also switch to new use of MBTR.raw

--- regression.py
+++ regression.py
@@ -42,6 +42,8 @@ def train_model(data, spec, kernel_matrix):
 def train_and_predict(data, spec, rep, train, predict, target_property=None):
     """Train a KRR model and predict some property
 
+    This function is intended for use when the split of one dataset are investigated.
+
     Args:
         spec: ModelSpec
         data: Dataset or subclass
@@ -55,10 +57,10 @@ def train_and_predict(data, spec, rep, train, predict, target_property=None):
 
     """
 
-    kernel_matrix = kernel(spec, rep[train])
+    kernel_matrix = kernel(spec, rep.raw[train])
     model = train_model(data[train], spec, kernel_matrix)
 
-    kernel_valid = kernel(spec, rep.raw[train], rep[predict])
+    kernel_valid = kernel(spec, rep.raw[train], rep.raw[predict])
 
     if target_property is None:
         return model(kernel_valid)
@@ -67,8 +69,27 @@ def train_and_predict(data, spec, rep, train, predict, target_property=None):
         return convert(data[predict], pred, spec.data['property'], target_property)
 
 
-def loss(data, spec, rep, train, predict, lossf=qmts.mae, target_property=None):
-    """Compute loss for given ModelSpec and representation"""
+def loss(data, spec, rep, train, predict, lossf=qmts.loss, target_property=None):
+    """Compute loss for given ModelSpec and representation, using indices
+
+    This function is intended for use when the split of one dataset are investigated,
+    as opposed to the one below, where different datasets/reps are used for training
+    and prediction.
+
+    Args:
+        data: Dataset
+        spec: ModelSpec
+        rep: Representation of Dataset
+        train: Training indices
+        predict: Prediction indices
+        lossf: If present, compute this loss
+        target_property: If present, predict this property 
+                         (otherwise default to the one specified by spec)
+
+    Returns:
+        loss: Float or list, depending on lossf
+
+    """
 
     prediction = train_and_predict(data, spec, rep, train, predict, target_property=target_property)
 
@@ -78,3 +99,75 @@ def loss(data, spec, rep, train, predict, lossf=qmts.mae, target_property=None):
         true = data[predict].p[target_property]
 
     return lossf(true, prediction)
+
+
+
+def train_and_predict_with_datasets_and_reps(data_train, data_predict,
+                                             spec,
+                                             rep_train, rep_predict,
+                                             target_property=None):
+    """Train a KRR model and predict some property, not using indices
+
+    Args:
+        data_train: Dataset for training
+        data_predict: Dataset for prediction
+        spec: ModelSpec
+        rep_train: Representation of data_train
+        rep_predict: Representation of data_test
+        target_property: Optional, if set, predictions are converted to this property
+
+    Returns:
+        predictions: ndarray with predictions
+
+    """
+
+    kernel_matrix = kernel(spec, rep_train.raw)
+    model = train_model(data_train, spec, kernel_matrix)
+
+    kernel_valid = kernel(spec, rep_train.raw, rep_predict.raw)
+
+    if target_property is None:
+        return model(kernel_valid)
+    else:
+        pred = model(kernel_valid)
+        return convert(data_predict, pred, spec.data['property'], target_property)
+
+
+def loss_with_datasets_and_reps(data_train, data_predict,
+                                spec,
+                                rep_train, rep_predict,
+                                target_property=None, lossf=qmts.loss):
+
+    """Compute loss using explicit datasets and reps
+
+    This function is intended for use with different instances of Dataset
+    and representations, for instance for keeping training and test data
+    separated.
+
+    Args:
+        data_train: Dataset for training
+        data_predict: Dataset for prediction
+        spec: ModelSpec
+        rep_train: Representation of data_train
+        rep_predict: Representation of data_test
+        lossf: If present, compute this loss
+        target_property: If present, predict this property 
+                         (otherwise default to the one specified by spec)
+
+    Returns:
+        loss: Float or list, depending on lossf
+
+    """
+
+    prediction = train_and_predict_with_datasets_and_reps(data_train, data_predict,
+                                                          spec,
+                                                          rep_train, rep_predict,
+                                                          target_property=target_property)
+
+    if target_property is None:
+        true = data_predict.p[spec.data['property']]
+    else:
+        true = data_predict.p[target_property]
+
+    return lossf(true, prediction)
+
