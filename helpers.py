68ac4684ad237c5b8697f91188485857f46bd886
Marcel Langer <me@sirmarcel.com>
2018-05-23 10:18:05 +0200

    Add dict replacement functions

--- helpers.py
+++ helpers.py
@@ -10,3 +10,31 @@ def convert_sequence(s):
             return (s[0], (s[1],))
         else:
             return (s[0], s[1:])
+
+
+def find_key_apply_f(d, key, f):
+    """Find key in dict, apply f to the value
+
+    Example:
+        d = {'arg1': 3,
+             'arg2': 2}
+
+        find_key_apply_f(d, 'arg2', lambda x: x**2) results in
+
+         d = {'arg1': 3,
+              'arg2': 4}
+
+    Note that this replacement happens inline.
+
+    """
+
+    for k, v in d.items():
+        if k == key:
+            d[k] = f(v)
+
+        if isinstance(v, dict):
+            find_key_apply_f(v, key, f)
+
+        if isinstance(v, list):
+            for i in v:
+                find_key_apply_f(i, key, f)
