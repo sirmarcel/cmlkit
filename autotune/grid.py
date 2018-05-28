53a0a5df644c782fc999f0f2f354e3a511cb0b07
Marcel Langer <me@sirmarcel.com>
2018-05-28 17:40:18 +0200

    Add a default medium grid

--- autotune/grid.py
+++ autotune/grid.py
@@ -5,3 +5,7 @@ def log2(start, stop, n):
     """Generate a 1-d grid in base 2 logspace."""
 
     return np.logspace(start, stop, num=n, base=2.0)
+
+
+def medium():
+    return np.logspace(-20, 20, num=41, base=2.0)
