c2195192c747692e83636e31ce044255f74b2354
Marcel Langer <me@sirmarcel.com>
2018-05-28 17:41:24 +0200

    autotune.core is now kinda functional

--- autotune/core.py
+++ autotune/core.py
@@ -1,73 +1,59 @@
-import qmmltools.indices as qmti
+import numpy as np
+import logging
+import time
 import qmmltools.inout as qmtio
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+import qmmltools.autoload as qmta
+from hyperopt import fmin, tpe, Trials
 from hyperopt.mongoexp import MongoTrials
 from qmmltools.autotune.objective import objective
-from qmmltools.autotune.settings import parse_settings
+from qmmltools.autotune.parse import preprocess
+from qmmltools.model_spec import ModelSpec
 
 
 def run(task):
     if isinstance(task, str):
-        task = qmtio.read_yaml(task_file)
+        task = qmtio.read_yaml(task)
 
-    # Pre-process task settings
-    parse_settings(task)
+    preprocess(task)
 
-    splits = generate_cv_idx(task['config']['cv'])
-
-    if task['config']['parallel'] = True:
+    if task['config']['parallel'] is True:
         trials = MongoTrials(task['config']['db'], exp_key=task['name'])
         logging.info('Performing parallel run. Remember to start workers!')
     else:
         trials = Trials()
         logging.info('Performing serial run.')
 
-    trials, duration = run_hypertune(task, task['config']['n_calls'], trials)
+    trials, duration = run_hyperopt(task, trials)
+
+    postprocess(task, trials, duration)
 
 
-
-
-def post(trials, duration, task):
+def postprocess(task, trials, duration):
     to_save = {
-        'db': db,
         'final_loss': trials.best_trial['result']['loss'],
         'final_loss_variance': trials.best_trial['result']['loss_variance'],
         'duration': duration,
         'post': {}
     }
 
-    qmtio.save('out/' + d['name'] + '.run', to_save)
+    qmtio.save('out/' + task['name'] + '.run', to_save)
 
-    top_3 = (np.array(trials.losses())).argsort()[:3]
+    top_2 = (np.array(trials.losses())).argsort()[:2]
 
-    for i in range(3):
-        spec = ModelSpec.from_dict(trials.trials[top_3[i]]['result']['spec_dict'])
+    for i in range(2):
+        spec = ModelSpec.from_dict(trials.trials[top_2[i]]['result']['spec_dict'])
         spec.name += '-' + 'best' + str(i)
         spec.save('out/')
 
 
-
-def generate_cv_idx(cv_config, n):
-
-    if cv['type'] == 'random':
-        splits = [qmti.threeway_split(n,
-                                      cv_config['n_train'],
-                                      cv_config['n_valid']) for i in range(cv_config['n_cv'])]
-
-    else:
-        raise NotImplementedError("Currently only random CV is supported.")
-
-    return splits
-
-
-def run_hyperopt(space, n_calls, trials):
+def run_hyperopt(task, trials):
 
     logging.info('Starting optimisation.')
     start = time.time()
     best = fmin(objective,
-                space=space,
+                space=task,
                 algo=tpe.suggest,
-                max_evals=n_calls,
+                max_evals=task['config']['n_calls'],
                 trials=trials,
                 verbose=1)
     end = time.time()
