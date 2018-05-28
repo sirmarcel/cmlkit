89a9028ed0ac0990043e5b36d5f581d4efcd641f
Marcel Langer <me@sirmarcel.com>
2018-05-28 17:41:04 +0200

    Objective function now has to do its own splits, poor thing

--- autotune/objective.py
+++ autotune/objective.py
@@ -1,9 +1,11 @@
 import numpy as np
 import logging
+from hyperopt import STATUS_OK
 from qmmltools.model_spec import ModelSpec
 import qmmltools.autoload
 from qmmltools.mbtr.cached_mbtr import DiskAndMemCachedMBTR
 import qmmltools.regression as qmtr
+import qmmltools.indices as qmti
 
 
 def objective(d):
@@ -21,11 +23,11 @@ def objective(d):
     rep = DiskAndMemCachedMBTR(dataset, spec)
     logging.debug('Successfully computed the MBTR!')
 
-    splits = internal['splits']
+    splits = generate_cv_idx(config['cv'], dataset.n)
 
     lossvec = []
     for rest, train, test in splits:
-        loss = qmtr.loss(dataset, spec, rep, train, predict, config['loss'],
+        loss = qmtr.loss(dataset, spec, rep, train, test, config['loss'],
                          target_property=data['property'])
 
         lossvec.append(loss)
@@ -39,5 +41,18 @@ def objective(d):
         'loss': mean_loss,
         'loss_variance': var_loss,
         'status': STATUS_OK,
-        'spec_dict': args['spec']
+        'spec_dict': d['spec']
     }
+
+
+def generate_cv_idx(cv_config, n):
+
+    if cv_config['type'] == 'random':
+        splits = [qmti.threeway_split(n,
+                                      cv_config['n_train'],
+                                      cv_config['n_valid']) for i in range(cv_config['n_cv'])]
+
+    else:
+        raise NotImplementedError("Currently only random CV is supported.")
+
+    return splits
