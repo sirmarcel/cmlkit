896171c0a00b101bb72d89c1434697edf87b59d4
Marcel Langer <me@sirmarcel.com>
2018-05-29 16:16:21 +0200

    Add convenience import

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from qmmltools.mbtr.mbtr import MBTR
 from qmmltools.model_spec import ModelSpec
 from qmmltools.dataset import Dataset, Subset
 from qmmltools.autotune.core import run_autotune
+from qmmltools.autoload import load_dataset
