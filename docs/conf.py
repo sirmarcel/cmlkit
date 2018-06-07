d6d6da8aede93b219c8508b0566987dad8ab6021
Marcel Langer <me@sirmarcel.com>
2018-06-07 14:58:33 +0200

    Mock qmmlpack import for read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,18 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+# Mock libraries that can't be installed via conda
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['qmmlpack']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ------------------------------------------------
 
