6bf70ab73736f9e42b976857cc859643d48bfaaf
Marcel Langer <me@sirmarcel.com>
2018-06-07 13:50:09 +0200

    Add __about__.py and read it in the docs

---
+++ cmlkit/__about__.py
@@ -0,0 +1,39 @@
+import os.path
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__commit__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+
+try:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    base_dir = None
+
+
+__title__ = "cmlkit"
+__summary__ = "Tools for working with qmmlpack"
+__uri__ = "https://github.com/sirmarcel/cmlkit"
+
+__version__ = "1.0.dev1"
+__short_version__ = "1.0"
+
+if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
+    with open(os.path.join(base_dir, ".commit")) as fp:
+        __commit__ = fp.read().strip()
+else:
+    __commit__ = None
+
+__author__ = "Marcel Langer"
+__email__ = "mail@sirmarcel.org"
+
+__license__ = "MIT License"
+__copyright__ = "2018 %s" % __author__
