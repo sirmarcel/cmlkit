1f1af1d14d337efa372aacc6b36627b6af4074e1
Marcel Langer <me@sirmarcel.com>
2019-04-11 17:50:55 +0200

    Add test for deserialisation

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,6 +3,13 @@
 from unittest import TestCase
 
 
-class TestImport(TestCase):
-    def test_import(self):
+class TestImports(TestCase):
+    def test_import_cmlkit(self):
         import cmlkit
+
+    def test_instantiation(TestCase):
+        import cmlkit
+
+        cmlkit.from_config(
+            {"kind": "krr", "config": {"nl": 1.0, "kernel": ["gaussian", 1.0]}}
+        )
