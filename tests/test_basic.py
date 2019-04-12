77a70e2b18dd717b711056781e2a5739e7cf22d1
Marcel Langer <me@sirmarcel.com>
2019-04-12 12:32:02 +0200

    Extend basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,17 +1,33 @@
 # Does anything work at all?
 
 from unittest import TestCase
+import pathlib
+import shutil
 
 
 class TestImports(TestCase):
     def test_import_cmlkit(self):
         import cmlkit
 
-    def test_instantiation(TestCase):
+
+class TestSerialization(TestCase):
+    def setUp(self):
+        self.tmpdir = (pathlib.Path(__file__) / "..").resolve() / "tmp_test_basic_serial"
+        self.tmpdir.mkdir(exist_ok=True)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_serialization(self):
         import cmlkit
 
         krr = cmlkit.from_config(
-            {"kind": "krr", "config": {"nl": 1.0, "kernel": ["gaussian", 1.0]}}
+            {"kind": "krr", "config": {"nl": 1.0, "kernel": ["gaussian", [1.0]]}}
         )
 
         krr.get_config()
+
+        cmlkit.save_yaml(self.tmpdir / 'krr.yml', krr.get_config())
+        krr2 = cmlkit.from_yaml(self.tmpdir / 'krr.yml')
+
+        self.assertEqual(krr2.get_config(), krr.get_config())
