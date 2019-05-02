341a50670569b91b96000a3fab14c5860b3511c6
Marcel Langer <me@sirmarcel.com>
2019-05-02 06:42:09 +0200

    Refactor base classes

--- cmlkit/engine/base_classes.py
+++ cmlkit/engine/base_classes.py
@@ -1,64 +1,112 @@
+"""Base classes.
+
+The main objects in cmlkit follow a pattern similar to Keras models:
+
+They can be serialised into and instantiated from `config` dictionaries.
+
+The underlying idea is that these objects are essentially convenience wrappers
+around functions with lots of arguments (for instance a regression method or
+a representation), and don't have a lot of state (ideall they'd have none), so
+once instantiated, they simply do some computation on inputs.
+
+The config dict then simply specifies what kind of computation is done.
+
+Since sometimes it is necessary to specify some arguments that do not influence
+the outcome of the computation, but determinse how it's executed (for instance
+how many cores to use, or cache settings), cmlkit objects also have a `context`,
+another dictionary that is passed to the constructor as optional keyword argument.
+
+So when implementing custom classes, the rules are:
+
+a) The config must fully determine the output,
+b) Everything else goes in the context.
+
+Why am I doing things this way? Here are some reasons:
+
+- This gives a consistent way to share model architectures as plaintext (yaml)
+- This makes hashing and caching a breeze (no trying to hash a complex object)
+- It gives full control over serialisation (I don't understand pickling at all)
+
+Also, in the future, this will make it relatively easy to provide a fully functional
+interface that avoids side effects.
+
+"""
+
 from cmlkit import logger
 from cmlkit.engine.inout import save_yaml
 
 
-class Configurable():
-    """Mixing class for serialising/de-serialising objects as dictionaries"""
+class Configurable:
+    """Mixin for serialising/de-serialising objects as dictionaries.
 
+    The goal is to provide a simple, robust, human-readable way to
+    pass objects around that have little to no state and are mainly
+    containers for a bunch of arguments/parameters.
+
+    This representation is the *config*, a tree-structured
+    nested dictionary. It's core structure is:
+
+    ```
+    {
+    "kind": "class_name",
+    "config": { ... },
+    }
+    ```
+
+    "kind" will be used to look up the class to instantiate, and the
+    "config" dict will be used to then instantiate. Typically, this will
+    be some variation on calling `__init__(**config)`.
+
+    The term "kind" is used because class is a keyword in Python.
+
+    """
 
     @classmethod
     def from_config(cls, config, **kwargs):
-        """Instantiate this class from config
+        """Instantiate this class from config."""
 
-        # TODO proper docstring
+        # Note: We retain the **kwargs for future flexibility,
+        # at the moment it is only needed to pass along the context.
 
-        """
-        if 'config' in config and 'kind' in config:
+        if "config" in config and "kind" in config:
             # we have been handed a fully formed config, not one for this class
-            return cls.from_config(config['config'], **kwargs)
+            return cls.from_config(config["config"], **kwargs)
         else:
             return cls._from_config(config, **kwargs)
 
     @classmethod
     def _from_config(cls, config, **kwargs):
-        # TODO: figure out how to avoid passing useless keyword args along
-        # currently every _from_config must accept pointless keyword arguments
-        # this method must be implemented
-        # and instantiate this object
-        raise NotImplementedError('Configurables must implement a _from_config method.')
+        return cls(**config, **kwargs)
 
     def get_config(self):
         """Return a dictionary describing this component"""
-        return {'kind': self.get_kind(), 'config': self._get_config()}
+        return {"kind": self.get_kind(), "config": self._get_config()}
 
     def get_kind(self):
+        """Return the class identifier."""
         # 'kind' is used to identify components
         # for de-serialisation; by default this will
         # be the lowercase version of the class name,
         # or manually defined as class-level attribute 'kind'
-        return getattr(self.__class__, 'kind', self.__class__.__name__.lower())
+        return getattr(self.__class__, "kind", self.__class__.__name__.lower())
 
     def _get_config(self):
         # this method must return a dict that fully
         # described how to re-instantiate this component
-        raise NotImplementedError('Configurables must implement a get_config method')
-
-    def save_config(self, filename=None):
-        config = self.get_config()
-
-        if filename is None:
-            if hasattr(self, 'name'):
-                name = self.name
-            else:
-                name = self.get_kind()
-
-        save_yaml(filename, config)
-
-
+        raise NotImplementedError("Configurables must implement a get_config method")
 
 
 class BaseComponent(Configurable):
-    """Base class for cmlkit model components"""
+    """Base class for cmlkit components.
+
+    Provides the mechanism for passing context (see explanation above)
+    to objects.
+
+    All context variables must have a default value set in the `default_context`
+    of the class. This is to ensure that passing an empty context will never
+    result in an error.
+
+    """
 
     # define this in subclass if certain context variables are required
     default_context = {}
@@ -69,7 +117,7 @@ class BaseComponent(Configurable):
 
         # special case: if context includes a sub-dict
         # under the key that is the name of this class,
-        # use this context instead (important for nested things)
+        # use this context instead (important for nested instantiation)
         if self.get_kind() in context:
             # the context for this class is expanded into this
             # class' context, while retaining the full dict,
@@ -78,7 +126,11 @@ class BaseComponent(Configurable):
             # as well!
             context = {**context, **context[self.get_kind()]}
 
-        self.context = {**global_default_context, **self.__class__.default_context, **context}
+        self.context = {
+            **global_default_context,
+            **self.__class__.default_context,
+            **context,
+        }
 
         logger.debug(f"Context for {self.get_kind()} is {self.context}.")
 
---
+++ tests/test_engine_base_classes.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+from cmlkit.engine import BaseComponent, Configurable
+
+
+class MyClass(BaseComponent):
+    kind = "hello"
+
+    default_context = {"b": 1}
+
+    def __init__(self, a, context={}):
+        super().__init__(context=context)
+        self.a = a
+
+    def _get_config(self):
+        return {"a": self.a}
+
+
+class TestConfigurable(TestCase):
+    def test_from_config(self):
+        hello = MyClass.from_config({"a": 1})
+        self.assertEqual(hello.a, 1)
+
+    def test_to_config(self):
+        hello = MyClass(a=1)
+        self.assertEqual(hello.get_config(), {"kind": "hello", "config": {"a": 1}})
+
+
+class TestContext(TestCase):
+    def test_default(self):
+        hello = MyClass(a=1)
+        self.assertEqual(hello.context["b"], 1)
+
+    def test_not_default(self):
+        hello = MyClass(a=1, context={"b": 2})
+        self.assertEqual(hello.context["b"], 2)
