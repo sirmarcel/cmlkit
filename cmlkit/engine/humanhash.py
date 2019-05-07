9fa0df5e6d448a424ebb86573d43d5137e63a6dc
Marcel Langer <me@sirmarcel.com>
2019-05-07 10:52:43 +0200

    Reformat humanhash

--- cmlkit/engine/humanhash.py
+++ cmlkit/engine/humanhash.py
@@ -1,7 +1,10 @@
 """
 humanhash: Human-readable representations of digests.
 
-Adapted from https://github.com/zacharyvoase/humanhash, which is Unlicensed.
+Vendorised from https://github.com/zacharyvoase/humanhash, which is Unlicensed.
+
+I replaced the word list with the top 200 male and 56 female names
+of the 1900 British census.
 
 The simplest ways to use this module are the :func:`humanize` and :func:`uuid`
 functions. For tighter control over the output, see :class:`HumanHasher`.
@@ -12,7 +15,264 @@ import uuid as uuidlib
 from functools import reduce
 
 
-DEFAULT_WORDLIST = ('William', 'John', 'George', 'Thomas', 'James', 'Arthur', 'Frederick', 'Charles', 'Albert', 'Robert', 'Joseph', 'Alfred', 'Henry', 'Ernest', 'Harry', 'Harold', 'Edward', 'Walter', 'Frank', 'Herbert', 'Richard', 'Reginald', 'Percy', 'Leonard', 'Samuel', 'David', 'Sidney', 'Francis', 'Stanley', 'Fred', 'Cecil', 'Horace', 'Cyril', 'Wilfred', 'Sydney', 'Leslie', 'Norman', 'Edwin', 'Victor', 'Benjamin', 'Tom', 'Hector', 'Jack', 'Alexander', 'Edgar', 'Bertie', 'Eric', 'Philip', 'Clifford', 'Redvers', 'Baden', 'Bernard', 'Daniel', 'Donald', 'Ralph', 'Archibald', 'Stephen', 'Willie', 'Peter', 'Christopher', 'Hugh', 'Lewis', 'Douglas', 'Gilbert', 'Ronald', 'Isaac', 'Hubert', 'Maurice', 'Clarence', 'Lawrence', 'Michael', 'Edmund', 'Patrick', 'Percival', 'Andrew', 'Matthew', 'Evan', 'Wilfrid', 'Bertram', 'Louis', 'Arnold', 'Kenneth', 'Gordon', 'Ivor', 'Gerald', 'Abraham', 'Geoffrey', 'Owen', 'Raymond', 'Oliver', 'Claude', 'Alan', 'Mark', 'Jesse', 'Reuben', 'Roland', 'Lionel', 'Alec', 'Charlie', 'Howard', 'Sam', 'Morris', 'Vincent', 'Dennis', 'Laurence', 'Martin', 'Joe', 'Allan', 'Jacob', 'Roberts', 'Rowland', 'Wallace', 'Bert', 'Anthony', 'Oswald', 'Frederic', 'Archie', 'Roy', 'Trevor', 'Colin', 'Clement', 'Jonathan', 'Joshua', 'Enoch', 'Leo', 'Basil', 'Ellis', 'Denis', 'Mary', 'Florence', 'Annie', 'Edith', 'Alice', 'Elizabeth', 'Elsie', 'Dorothy', 'Ethel', 'Doris', 'Margaret', 'Gladys', 'Sarah', 'Lilian', 'Ellen', 'Hilda', 'Lily', 'Winifred', 'Violet', 'Ada', 'Emily', 'Beatrice', 'Nellie', 'May', 'Mabel', 'Ivy', 'Rose', 'Gertrude', 'Jane', 'Catherine', 'Kathleen', 'Frances', 'Agnes', 'Olive', 'Jessie', 'Emma', 'Eva', 'Minnie', 'Maud', 'Louisa', 'Amy', 'Grace', 'Clara', 'Martha', 'Daisy', 'Evelyn', 'Hannah', 'Lucy', 'Kate', 'Eliza', 'Bertha', 'Ann', 'Eleanor', 'Harriet', 'Phyllis', 'Constance', 'Dora', 'Ida', 'Esther', 'Isabella', 'Nora', 'Marjorie', 'Laura', 'Charlotte', 'Irene', 'Ruth', 'Bessie', 'Caroline', 'Fanny', 'Muriel', 'Maggie', 'Edna', 'Norah', 'Amelia', 'Helen', 'Mildred', 'Vera', 'Gwendoline', 'Eveline', 'Lizzie', 'Marion', 'Rachel', 'Rosina', 'Florrie', 'Maria', 'Lydia', 'Ruby', 'Victoria', 'Miriam', 'Blanche', 'Rosa', 'Rebecca', 'Julia', 'Ella', 'Henrietta', 'Isabel', 'Matilda', 'Janet', 'Phoebe', 'Susan', 'Millicent', 'Audrey', 'Rhoda', 'Myra', 'Nelly', 'Selina', 'Margery', 'Georgina', 'Helena', 'Leah', 'Susannah', 'Clarice', 'Cecilia', 'Eileen', 'Christina', 'Barbara', 'Pretoria', 'Lena', 'Lillie', 'Flora', 'Freda', 'Josephine', 'Marian', 'Adelaide', 'Jennie', 'Enid', 'Sophia', 'Lillian')
+DEFAULT_WORDLIST = (
+    "William",
+    "John",
+    "George",
+    "Thomas",
+    "James",
+    "Arthur",
+    "Frederick",
+    "Charles",
+    "Albert",
+    "Robert",
+    "Joseph",
+    "Alfred",
+    "Henry",
+    "Ernest",
+    "Harry",
+    "Harold",
+    "Edward",
+    "Walter",
+    "Frank",
+    "Herbert",
+    "Richard",
+    "Reginald",
+    "Percy",
+    "Leonard",
+    "Samuel",
+    "David",
+    "Sidney",
+    "Francis",
+    "Stanley",
+    "Fred",
+    "Cecil",
+    "Horace",
+    "Cyril",
+    "Wilfred",
+    "Sydney",
+    "Leslie",
+    "Norman",
+    "Edwin",
+    "Victor",
+    "Benjamin",
+    "Tom",
+    "Hector",
+    "Jack",
+    "Alexander",
+    "Edgar",
+    "Bertie",
+    "Eric",
+    "Philip",
+    "Clifford",
+    "Redvers",
+    "Baden",
+    "Bernard",
+    "Daniel",
+    "Donald",
+    "Ralph",
+    "Archibald",
+    "Stephen",
+    "Willie",
+    "Peter",
+    "Christopher",
+    "Hugh",
+    "Lewis",
+    "Douglas",
+    "Gilbert",
+    "Ronald",
+    "Isaac",
+    "Hubert",
+    "Maurice",
+    "Clarence",
+    "Lawrence",
+    "Michael",
+    "Edmund",
+    "Patrick",
+    "Percival",
+    "Andrew",
+    "Matthew",
+    "Evan",
+    "Wilfrid",
+    "Bertram",
+    "Louis",
+    "Arnold",
+    "Kenneth",
+    "Gordon",
+    "Ivor",
+    "Gerald",
+    "Abraham",
+    "Geoffrey",
+    "Owen",
+    "Raymond",
+    "Oliver",
+    "Claude",
+    "Alan",
+    "Mark",
+    "Jesse",
+    "Reuben",
+    "Roland",
+    "Lionel",
+    "Alec",
+    "Charlie",
+    "Howard",
+    "Sam",
+    "Morris",
+    "Vincent",
+    "Dennis",
+    "Laurence",
+    "Martin",
+    "Joe",
+    "Allan",
+    "Jacob",
+    "Roberts",
+    "Rowland",
+    "Wallace",
+    "Bert",
+    "Anthony",
+    "Oswald",
+    "Frederic",
+    "Archie",
+    "Roy",
+    "Trevor",
+    "Colin",
+    "Clement",
+    "Jonathan",
+    "Joshua",
+    "Enoch",
+    "Leo",
+    "Basil",
+    "Ellis",
+    "Denis",
+    "Mary",
+    "Florence",
+    "Annie",
+    "Edith",
+    "Alice",
+    "Elizabeth",
+    "Elsie",
+    "Dorothy",
+    "Ethel",
+    "Doris",
+    "Margaret",
+    "Gladys",
+    "Sarah",
+    "Lilian",
+    "Ellen",
+    "Hilda",
+    "Lily",
+    "Winifred",
+    "Violet",
+    "Ada",
+    "Emily",
+    "Beatrice",
+    "Nellie",
+    "May",
+    "Mabel",
+    "Ivy",
+    "Rose",
+    "Gertrude",
+    "Jane",
+    "Catherine",
+    "Kathleen",
+    "Frances",
+    "Agnes",
+    "Olive",
+    "Jessie",
+    "Emma",
+    "Eva",
+    "Minnie",
+    "Maud",
+    "Louisa",
+    "Amy",
+    "Grace",
+    "Clara",
+    "Martha",
+    "Daisy",
+    "Evelyn",
+    "Hannah",
+    "Lucy",
+    "Kate",
+    "Eliza",
+    "Bertha",
+    "Ann",
+    "Eleanor",
+    "Harriet",
+    "Phyllis",
+    "Constance",
+    "Dora",
+    "Ida",
+    "Esther",
+    "Isabella",
+    "Nora",
+    "Marjorie",
+    "Laura",
+    "Charlotte",
+    "Irene",
+    "Ruth",
+    "Bessie",
+    "Caroline",
+    "Fanny",
+    "Muriel",
+    "Maggie",
+    "Edna",
+    "Norah",
+    "Amelia",
+    "Helen",
+    "Mildred",
+    "Vera",
+    "Gwendoline",
+    "Eveline",
+    "Lizzie",
+    "Marion",
+    "Rachel",
+    "Rosina",
+    "Florrie",
+    "Maria",
+    "Lydia",
+    "Ruby",
+    "Victoria",
+    "Miriam",
+    "Blanche",
+    "Rosa",
+    "Rebecca",
+    "Julia",
+    "Ella",
+    "Henrietta",
+    "Isabel",
+    "Matilda",
+    "Janet",
+    "Phoebe",
+    "Susan",
+    "Millicent",
+    "Audrey",
+    "Rhoda",
+    "Myra",
+    "Nelly",
+    "Selina",
+    "Margery",
+    "Georgina",
+    "Helena",
+    "Leah",
+    "Susannah",
+    "Clarice",
+    "Cecilia",
+    "Eileen",
+    "Christina",
+    "Barbara",
+    "Pretoria",
+    "Lena",
+    "Lillie",
+    "Flora",
+    "Freda",
+    "Josephine",
+    "Marian",
+    "Adelaide",
+    "Jennie",
+    "Enid",
+    "Sophia",
+    "Lillian",
+)
 
 
 class HumanHasher(object):
@@ -35,7 +295,7 @@ class HumanHasher(object):
             raise ValueError("Wordlist must have exactly 256 items")
         self.wordlist = wordlist
 
-    def humanize(self, hexdigest, words=4, separator='-'):
+    def humanize(self, hexdigest, words=4, separator="-"):
 
         """
         Humanize a given hexadecimal digest.
@@ -49,7 +309,9 @@ class HumanHasher(object):
         """
 
         # Gets a list of byte values between 0-255.
-        bbytes = list(map(lambda x: int(x, 16), map(''.join, zip(hexdigest[::2], hexdigest[1::2]))))
+        bbytes = list(
+            map(lambda x: int(x, 16), map("".join, zip(hexdigest[::2], hexdigest[1::2])))
+        )
         # Compress an arbitrary number of bbytes to `words`.
         compressed = self.compress(bbytes, words)
         # Map the compressed byte values through the word list.
@@ -80,10 +342,9 @@ class HumanHasher(object):
 
         # Split `bbytes` into `target` segments.
         seg_size = length // target
-        segments = [bbytes[i * seg_size:(i + 1) * seg_size]
-                    for i in range(target)]
+        segments = [bbytes[i * seg_size : (i + 1) * seg_size] for i in range(target)]
         # Catch any left-over bbytes in the last segment.
-        segments[-1].extend(bbytes[target * seg_size:])
+        segments[-1].extend(bbytes[target * seg_size :])
 
         # Use a simple XOR checksum-like function for compression.
         checksum = lambda bbytes: reduce(operator.xor, bbytes, 0)
@@ -99,7 +360,7 @@ class HumanHasher(object):
         as :meth:`humanize` (they'll be passed straight through).
         """
 
-        digest = str(uuidlib.uuid4()).replace('-', '')
+        digest = str(uuidlib.uuid4()).replace("-", "")
         return self.humanize(digest, **params), digest
 
 
