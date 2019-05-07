cf65c1a25785a41a501ae46a1dca49b5cf21538b
Marcel Langer <me@sirmarcel.com>
2019-05-07 11:41:34 +0200

    Light refactor of engine.inout

--- cmlkit/engine/inout.py
+++ cmlkit/engine/inout.py
@@ -4,32 +4,50 @@ from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
 
+def normalize_extension(path, extension):
+    """If the path doesn't have the extension, add it."""
+    p = Path(path)
+    return p.with_suffix(extension)
+
+
 def makedir(p):
+    """Create directory at path and its parents.
+
+    Args:
+        p: Path-like object
+    """
     path = Path(p)
     path.mkdir(exist_ok=True, parents=True)
 
 
 def save_npy(filename, d):
-    """Save a dictionary with numpy"""
-    filename = str(filename)
-    filename.replace('.npy', '')
-    np.save(filename, d)
+    """Save a dictionary with numpy.
+
+    Args:
+        filename: Path-like object. (Extension not required.)
+        d: Dict to save.
+    """
+    np.save(normalize_extension(filename, ".npy"), d)
 
 
 def safe_save_npy(filename, d):
-    """Save a dict with numpy using a separate Thread
+    """Save a dict with numpy using a separate Thread.
 
     This is important because otherwise corrupted files are written to disk,
     when the pickler gets interrupted mid-write.
+
+    Args:
+        filename: Path-like object. (Extension not required.)
+        d: Dict to save.
     """
     with ThreadPoolExecutor(max_workers=1) as executor:
         executor.submit(save_npy, filename, d)
 
 
 def read_npy(filename):
-    """Load numpy data stored in filename"""
+    """Load numpy data stored in filename."""
 
-    d = np.load(filename)
+    d = np.load(normalize_extension(filename, ".npy"))
 
     if d.size == 1:
         return d.item()
@@ -45,26 +63,20 @@ def save_yaml(filename, d):
     flowstyle, i.e. inline.
 
     Args:
-        filename: Path to file
-        d: Dict to save
-        flowstyle: Bool indicating whether dicts are saved compactly or not
-
-    Returns: None
+        filename: Path to file. (Extension not required.)
+        d: Dict to save.
 
     """
-    filename = str(filename)
-    filename.replace('.yml', '')
 
     # Define representer for lists/tuples
     def sequence_representer(dumper, data):
-        return dumper.represent_sequence(u'tag:yaml.org,2002:seq', data, flow_style=True)
+        return dumper.represent_sequence(u"tag:yaml.org,2002:seq", data, flow_style=True)
 
     # Define representer for numpy floats
     def float_representer(dumper, data):
         return dumper.represent_float(data)
 
     # Define representer for numpy ints
-
     def int_representer(dumper, data):
         return dumper.represent_int(data)
 
@@ -74,15 +86,14 @@ def save_yaml(filename, d):
     yaml.add_representer(np.float64, float_representer)
     yaml.add_representer(np.int64, int_representer)
 
-    with open(filename, 'w') as outfile:
+    with open(normalize_extension(filename, ".yml"), "w") as outfile:
         yaml.dump(d, outfile, default_flow_style=False)
 
 
 def read_yaml(filename):
-    """Read yaml dictionary from file."""
+    """Read yaml dictionary from filename."""
 
-    stream = open(filename, 'r')
-    d = yaml.load(stream)
-    stream.close()
+    with open(normalize_extension(filename, ".yml"), "r") as stream:
+        d = yaml.safe_load(stream)
 
     return d
