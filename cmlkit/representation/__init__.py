13686b45a5807bb36812889429ffae31761da0c9
Marcel Langer <me@sirmarcel.com>
2019-05-17 17:02:16 +0200

    Temporary changes while I refactor representations

--- cmlkit/representation/__init__.py
+++ cmlkit/representation/__init__.py
@@ -1,4 +1,5 @@
-from .mbtr import MBTR1, MBTR2, MBTR3, MBTR4
+# from .mbtr import MBTR1, MBTR2, MBTR3, MBTR4
+from .representation import Representation
 from .composed import ComposedRepresentation
 from .only_coords import OnlyCoords
 from .only_dists import OnlyDists, OnlyDistsHistogram
@@ -7,10 +8,10 @@ from .symmetry_functions import BasicSymmetryFunctions, EmpiricalSymmetryFunctio
 from .soap import Soap
 
 classes = {
-    MBTR1.kind: MBTR1,
-    MBTR2.kind: MBTR2,
-    MBTR3.kind: MBTR3,
-    MBTR4.kind: MBTR4,
+    # MBTR1.kind: MBTR1,
+    # MBTR2.kind: MBTR2,
+    # MBTR3.kind: MBTR3,
+    # MBTR4.kind: MBTR4,
     ComposedRepresentation.kind: ComposedRepresentation,
     OnlyCoords.kind: OnlyCoords,
     OnlyDists.kind: OnlyDists,
