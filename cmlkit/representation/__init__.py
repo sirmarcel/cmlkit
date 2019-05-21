9b28ce365a6aee36506dcc17237e52b735d15a43
Marcel Langer <me@sirmarcel.com>
2019-05-21 15:41:27 +0200

    Clean up representation imports

--- cmlkit/representation/__init__.py
+++ cmlkit/representation/__init__.py
@@ -1,12 +1,17 @@
-# from .mbtr import MBTR1, MBTR2, MBTR3, MBTR4
 from .representation import Representation
 from .composed import Composed
 from .soap import SOAP
 from .mbtr import MBTR1, MBTR2, MBTR3, MBTR4
 from .sf import SymmetryFunctions
+from .coulomb_matrix import CoulombMatrix
 
 from .soap import classes as classes_soap
 from .mbtr import classes as classes_mbtr
 from .sf import classes as classes_sf
 
-classes = {**classes_soap, **classes_mbtr, **classes_sf}
+classes = {
+    **classes_soap,
+    **classes_mbtr,
+    **classes_sf,
+    CoulombMatrix.kind: CoulombMatrix,
+}
