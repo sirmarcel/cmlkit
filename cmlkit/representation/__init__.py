46f0c03936a85b176b7e56653ad42cd3a7951a66
Marcel Langer <me@sirmarcel.com>
2019-05-21 15:04:29 +0200

    Remove more things

--- cmlkit/representation/__init__.py
+++ cmlkit/representation/__init__.py
@@ -1,10 +1,6 @@
 # from .mbtr import MBTR1, MBTR2, MBTR3, MBTR4
 from .representation import Representation
 from .composed import ComposedRepresentation
-from .only_coords import OnlyCoords
-from .only_dists import OnlyDists, OnlyDistsHistogram
-from .coulomb_matrix import CoulombMatrixMinimal
-from .symmetry_functions import BasicSymmetryFunctions, EmpiricalSymmetryFunctions
 # from .soap import Soap
 
 classes = {
@@ -13,10 +9,10 @@ classes = {
     # MBTR3.kind: MBTR3,
     # MBTR4.kind: MBTR4,
     ComposedRepresentation.kind: ComposedRepresentation,
-    OnlyCoords.kind: OnlyCoords,
-    OnlyDists.kind: OnlyDists,
-    OnlyDistsHistogram.kind: OnlyDistsHistogram,
-    CoulombMatrixMinimal.kind: CoulombMatrixMinimal,
-    BasicSymmetryFunctions.kind: BasicSymmetryFunctions,
-    EmpiricalSymmetryFunctions.kind: EmpiricalSymmetryFunctions,
+    # OnlyCoords.kind: OnlyCoords,
+    # OnlyDists.kind: OnlyDists,
+    # OnlyDistsHistogram.kind: OnlyDistsHistogram,
+    # CoulombMatrixMinimal.kind: CoulombMatrixMinimal,
+    # BasicSymmetryFunctions.kind: BasicSymmetryFunctions,
+    # EmpiricalSymmetryFunctions.kind: EmpiricalSymmetryFunctions,
 }
