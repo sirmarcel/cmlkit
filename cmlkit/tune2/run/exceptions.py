92b19d02cd8ef0b84601ce72d0d978683221d831
Marcel Langer <me@sirmarcel.com>
2019-05-31 14:10:13 +0200

    Add rudimentary exception de/serialisation

---
+++ cmlkit/tune2/run/exceptions.py
@@ -0,0 +1,34 @@
+"""Exceptions that can be automatically caught by tune.
+
+As far as I know, there is no straightforward way to properly serialise exceptions.
+So we're doing it my hand for now -- open to suggestions on how to do it better.
+"""
+
+from qmmlpack import QMMLException
+from concurrent.futures import TimeoutError
+
+exceptions = {
+    "QMMLException": QMMLException,
+    "TimeoutError": TimeoutError,
+    "ValueError": ValueError,
+    "AssertionError": AssertionError,
+    "RuntimeError": RuntimeError,
+    "Exception": Exception,  # this will catch basically everything
+}
+
+
+def get_exceptions(args):
+    result = []
+    for a in args:
+        if a in exceptions:
+            result.append(exceptions[a])
+        else:
+            raise ValueError(
+                f"cmlkit.tune can currently not catch {a}. Please file an issue."
+            )
+
+    return result
+
+
+def get_exceptions_spec(args):
+    return [a.__name__ for a in args]
---
+++ tests/test_tune_run_exceptions.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from concurrent.futures import TimeoutError
+
+from cmlkit.tune2.run.exceptions import get_exceptions, get_exceptions_spec, exceptions
+
+
+class TestRunExceptions(TestCase):
+    def test_deserialisation(self):
+        result = get_exceptions(["TimeoutError", "ValueError"])
+
+        self.assertEqual(result, [TimeoutError, ValueError])
+
+        with self.assertRaises(ValueError):
+            get_exceptions([OSError])
+
+    def test_roundtrip(self):
+        self.assertEqual(
+            get_exceptions(get_exceptions_spec(list(exceptions.values()))),
+            list(exceptions.values()),
+        )
